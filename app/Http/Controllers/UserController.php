b2ed9e00fa39c5966c79fc922343b4b58a78c2b8
Kenc1k <iskandarisabrkov@gmail.com>
2024-11-19 11:41:50 +0500

    "something in your smile"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        echo "Karochi ravhsan siz menga ishonin menga ishonsez boli";
     }
 
     /**
